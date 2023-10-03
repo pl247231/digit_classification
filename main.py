e3403b2f3f6ab36de4bcb7ec5a82ea535a0e70d7
Alan Wu <70585668+pl247231@users.noreply.github.com>
2023-10-02 21:26:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,59 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
-import os
+import numpy as np
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Dense
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis = 1)
+class Model(tf.keras.Model):
+    def __init__(self) -> None:
+        super(Model, self).__init__()
+        self.layer0 = tf.keras.layers.Flatten(input_shape = (28*28,))
+        self.layer1 = tf.keras.layers.Dense(512,activation='relu')
+        self.layer2 = tf.keras.layers.Dense(10,activation='softmax')
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(len(x_train[0]), len(x_train[0][0]))))
-model.add(tf.keras.layers.Dense(128,activation='relu'))
-model.add(tf.keras.layers.Dense(128,activation='relu'))
-model.add(tf.keras.layers.Dense(10,activation='softmax'))
+    def call(self, input: np.array) -> tf.Tensor:
+        input = self.layer0(input)
+        input = self.layer1(input)
+        input = self.layer2(input)
+        return input
 
-model.compile(optimizer='adam',loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics="accuracy")
-
-model.fit(x_train,y_train,epochs=4)
-model.save('digit_classify')
-loss, accuracy = model.evaluate(x_test,y_test)
-print(str(loss) + '\n' + str(accuracy))
-model = tf.keras.models.load_model('digit_classify')
-img_num = 0
-num_right = 0
-while os.path.isfile("Numbers/" + str(img_num) + ".png"):
-    img = cv2.imread("Numbers/" + str(img_num) + ".png",cv2.IMREAD_GRAYSCALE)
-    img = np.invert(np.array([img]))
-    prediction = model.predict(img)
-    result = np.argmax(prediction)
-    if result == img_num:
-        num_right+=1
-
-    plt.imshow(img[0],cmap=plt.cm.binary)
-    plt.show()
-    img_num+=1
-print(num_right/10)
-
+def one_hot_encoding(input: np.array) -> np.array:
+    unique = set(input)
+    num_unique = len(set(input))
+    #Create a dict to map input category to one hot index
+    num_to_pos = {}
+    index = 0
+    for value in unique:
+      num_to_pos[value] = index
+      index+=1
+    one_hot = []
+    #One hot encode each input value
+    for value in input:
+      arr = np.array([0]*len(unique))
+      arr[num_to_pos[value]] = 1
+      one_hot.append(arr)
+    return np.array(one_hot)
 
 
+data = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = data
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype("float32") / 255
+
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype("float32") / 255
+
+
+loss_func = 'categorical_crossentropy'
+
+optimizer = 'adam'
+model = Model()
+model.compile(optimizer = optimizer, loss = loss_func, metrics = ["accuracy"])
+
+tf_train_labels = to_categorical(train_labels)
+tf_test_labels = to_categorical(test_labels)
+model.fit(train_images, tf_train_labels, epochs = 5, batch_size = 128)
+test_loss, test_acc = model.evaluate(test_images, tf_test_labels)
+print('testAccuracy', test_acc)
